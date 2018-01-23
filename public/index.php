5299cca35416485ee2be7b7ae1e3bc43bd9e690e
yinminqian1 <2311871437@qq.com>
2018-01-23 15:30:54 +0800

    yo

--- public/index.php
+++ public/index.php
@@ -14,6 +14,7 @@ import("api/merchant");
 init();
 function iNIt()
 {
+    dd(2);
     session_start();
     parse_uri();
 }
@@ -28,6 +29,7 @@ function parse_uri()
 
     switch ($arr[0]) {
         case '':
+        dd(1);
             import('view/public/nav_home');
             break;
         case"home":
